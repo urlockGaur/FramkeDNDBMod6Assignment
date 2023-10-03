50ea607a1312c20edcc488694a10ffd0549f74fe
Anthony <thewarparmoury@gmail.com>
2023-10-03 17:24:11 -0500

    Mod6 Assignment set-up

---
+++ FramkeDNDBMod6Assignment/FramkeDNDBMod6Assignment/Models/Media.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FramkeDNDBMod6Assignment.Models
+{
+    public abstract class Media
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+
+        public abstract string Display();
+
+
+
+
+    }
+}
---
+++ FramkeDNDBMod6Assignment/FramkeDNDBMod6Assignment/Models/Show.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FramkeDNDBMod6Assignment.Models
+{
+    internal class Show
+    {
+    }
+}
